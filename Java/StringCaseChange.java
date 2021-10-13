a0c5d71b4a9e4a53ae1d1a60a95447720eacb24c
chinmaykulkarni11 <53442157+chinmaykulkarni11@users.noreply.github.com>
2021-10-13 20:18:28 +0530

    Update StringCaseChange.java

--- Java/StringCaseChange.java
+++ Java/StringCaseChange.java
@@ -1,1 +1,27 @@
 
+import java.util.Scanner;
+
+public class StringCaseChange {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String ip = sc.nextLine();
+		System.out.println(changeCase(ip));
+		sc.close();
+
+	}
+
+	public static String changeCase(String ip) {
+		StringBuilder b = new StringBuilder(ip);
+		for (int i = 0; i < b.length(); i++) {
+			if (b.charAt(i) >= 'a' && b.charAt(i) <= 'z') {
+				b.setCharAt(i, (char) (b.charAt(i) - 32));
+			} else if (b.charAt(i) >= 'A' && b.charAt(i) <= 'Z') {
+					b.setCharAt(i, (char) (b.charAt(i) + 32));
+			}
+		}
+		return String.valueOf(b);
+	}
+
+}
+
